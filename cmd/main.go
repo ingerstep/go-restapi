86c390d8c6b09a878066f916a57ee37477540ec1
ingerstep <stepan.karelin.1996@mail.ru>
2024-09-30 22:13:53 +0700

    feat(server): add handlers auth, item & list

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	gorestapi "github.com/ingerstep/go-restapi"
+	"github.com/ingerstep/go-restapi/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(gorestapi.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("3005", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
---
+++ user.go
@@ -0,0 +1,8 @@
+package gorestapi
+
+type User struct {
+	Id       int    `json:"-"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
