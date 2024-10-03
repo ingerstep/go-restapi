94b9c4c8333512da22e9f6a5fb0e22dbf2a13aa7
ingerstep <stepan.karelin.1996@mail.ru>
2024-10-03 14:09:45 +0700

    update(service): add todo item CRUD actions

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -19,6 +19,11 @@ type TodoList interface {
 }
 
 type TodoItem interface {
+	Create(listId int, item gorestapi.TodoItem) (int, error)
+	GetAll(userId, listId int) ([]gorestapi.TodoItem, error)
+	GetById(userId, itemId int) (gorestapi.TodoItem, error)
+	Delete(userId, itemId int) error
+	Update(userId, itemId int, input gorestapi.UpdateItemInput) error
 }
 
 type Repository struct {
@@ -31,5 +36,6 @@ func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{
 		Authorization: NewAuthPostgres(db),
 		TodoList:      NewTodoListPostgres(db),
+		TodoItem:      NewTodoItemPostgres(db),
 	}
 }
---
+++ pkg/service/todo_item.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	gorestapi "github.com/ingerstep/go-restapi"
+	"github.com/ingerstep/go-restapi/pkg/repository"
+)
+
+type TodoItemService struct {
+	repo     repository.TodoItem
+	listRepo repository.TodoList
+}
+
+func NewTodoItemService(repo repository.TodoItem, listRepo repository.TodoList) *TodoItemService {
+	return &TodoItemService{
+		repo:     repo,
+		listRepo: listRepo,
+	}
+}
+
+func (s *TodoItemService) Create(userId, listId int, item gorestapi.TodoItem) (int, error) {
+	_, err := s.listRepo.GetById(userId, listId)
+	if err != nil {
+		return 0, nil
+	}
+
+	return s.repo.Create(listId, item)
+}
+
+func (s *TodoItemService) GetAll(userId, listId int) ([]gorestapi.TodoItem, error) {
+	return s.repo.GetAll(userId, listId)
+}
+
+func (s *TodoItemService) GetById(userId, itemId int) (gorestapi.TodoItem, error) {
+	return s.repo.GetById(userId, itemId)
+}
+
+func (s *TodoItemService) Delete(userId, itemId int) error {
+	return s.repo.Delete(userId, itemId)
+}
+
+func (s *TodoItemService) Update(userId, itemId int, input gorestapi.UpdateItemInput) error {
+	return s.repo.Update(userId, itemId, input)
+}
